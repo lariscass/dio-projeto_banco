2de5ad203ddcee457cc80a53f0075c5b4e63b116
larissa <larissabro07@gmail.com>
2025-02-21 10:02:00 -0300

    commit finalização do projeto :)

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,28 @@
+
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        int numero = Integer.valueOf(args [0]);
+        String agencia = args [1];
+        String nomeCliente = args [2];
+        double saldo = Double.parseDouble(args[3]);
+
+        System.out.println("Por favor, digite o número da Agência");
+       // System.out.println(agencia);
+        System.out.println("Informe seu nome");
+        //System.out.println(nomeCliente);
+        System.out.println("Informe seu número");
+        System.out.println("Informe seu saldo");
+
+        System.out.println("Olá ".concat(nomeCliente)
+                .concat(", obrigado por criar uma conta em nosso banco, sua agência é ")
+                .concat(agencia)
+                .concat(", conta ")
+                .concat(String.valueOf(numero))
+                .concat(" e seu saldo ")
+                .concat(String.valueOf(saldo))
+                .concat(" já está disponível para saque."));
+
     }
 }
